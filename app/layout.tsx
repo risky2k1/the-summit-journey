13ba028a2b3cea1f5caf038c6ea57e9fbdc0dcf2
TuanPM1 <tuanpm1@kaopiz.com>
2026-03-20 18:05:15 +0700

    refactor: Update font import and usage in layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const jetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
 });
 
@@ -23,11 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="vi"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="vi" className="h-full mdl-js">
+      <body
+        className={`${jetBrainsMono.className} min-h-full flex flex-col antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
