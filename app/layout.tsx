c1489a5f1f3ab7f2949b9c7ba2b496a5be5eb0ce
TuanPM1 <tuanpm1@kaopiz.com>
2026-03-25 13:45:14 +0700

    feat: add speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const jetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
